cf32a6c59e510ccca040ed5f1b1626c3e29bb1b1
SKY <akash.rm.roy@gmail.com>
2020-05-08 22:07:30 +0530

    Add files via upload

--- CPPToUnity/CPPToUnity.cpp
+++ CPPToUnity/CPPToUnity.cpp
@@ -20,27 +20,28 @@ extern "C"
 
 		// create an opencv object sharing the same data space
 		Mat image(height, width, CV_8UC4, *rawImage);
-
+		
 		// start with flip (in both directions) if your image looks inverted
-		flip(image, image, -1);
+		//flip(image, image, -1);
 
 		// start processing the image
 		// ************************************************
-
+		
 		Mat edges;
 		Canny(image, edges, 50, 200);
 		dilate(edges, edges, (5, 5));
 		cvtColor(edges, edges, COLOR_GRAY2RGBA);
 		normalize(edges, edges, 0, 1, NORM_MINMAX);
 		multiply(image, edges, image);
-
+		
 		// end processing the image
 		// ************************************************
 
 		// flip again (just vertically) to get the right orientation
-		flip(image, image, 0);
-
+		//flip(image, image, 0);
+		
 		//Put image-processing algo here 
+		
 		return ++operationCount;
 	}
 }
